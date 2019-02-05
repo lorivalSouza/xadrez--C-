02b589e19f06894a90a6cd2007d66d5b8122d157
Lorival Souza <lorivalnew@gmail.com>
2019-02-05 03:31:29 -0200

    Comecando a colocar pecas no tabuleiro

---
+++ xadrez-console/xadrez/Rei.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using tabuleiro;
+using System.Text;
+
+namespace xadrez
+{
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
